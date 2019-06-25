5666e1da3beccad4a179353b5019a77206f20d09
Quentin.Duchemin <qduchemin9@gmail.com>
2019-06-25 04:33:20 +0200

    initial commit

---
+++ quizz/polynomes_endomorphismes.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  document.getElementById("non").style.backgroundColor ='##FF0000';
+    var x = document.getElementById("non").value;
+    document.getElementById("demo").innerHTML = x;
+}
+function myFunction2() {
+
+    document.getElementById("oui").style.backgroundColor ='#00FF00';
+    var x = document.getElementById("oui").value;
+    document.getElementById("demo").innerHTML = x;
+}
