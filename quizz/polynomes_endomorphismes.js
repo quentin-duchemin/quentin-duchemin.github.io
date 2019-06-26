79f4e2b32fd60ba912a28c3d8b29b42855ebc12a
Quentin.Duchemin <qduchemin9@gmail.com>
2019-06-26 05:34:33 +0200

    first quizz

--- quizz/polynomes_endomorphismes.js
+++ quizz/polynomes_endomorphismes.js
@@ -1,11 +1,14 @@
-function myFunction() {
-  document.getElementById("non").style.backgroundColor ='##FF0000';
-    var x = document.getElementById("non").value;
-    document.getElementById("demo").innerHTML = x;
+function myFunction(int) {
+  	document.getElementById("TRUE"+int).style.backgroundColor =  '#00FF00' ;
+  	document.getElementById("FALSE"+int).style.backgroundColor =  '#D3D3D3' ;
+    var x = document.getElementById("true"+int).value;
+    document.getElementById("demo"+int).innerHTML = x;
 }
-function myFunction2() {
+function myFunction2(int) {
 
-    document.getElementById("oui").style.backgroundColor ='#00FF00';
-    var x = document.getElementById("oui").value;
-    document.getElementById("demo").innerHTML = x;
+    document.getElementById("FALSE"+int).style.backgroundColor ='#FF0000';
+    document.getElementById("TRUE"+int).style.backgroundColor =  '#D3D3D3' ;
+
+    var x = document.getElementById("false"+int).value;
+    document.getElementById("demo"+int).innerHTML = x;
 }
