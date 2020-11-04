2a6059f79fe34f1473da486fae5ce8feb69c21c5
quentin-duchemin <quentin.duchemin@eleves.enpc.fr>
2020-11-04 23:04:54 +0100

    update

--- quizz/applications.js
+++ quizz/applications.js
@@ -9,10 +9,36 @@ function myFunction(int) {
 function myFunction2(int) {
 
     document.getElementById("FALSE"+int).style.backgroundColor ='#FF0000';
-    document.getElementById("TRUE"+int).style.backgroundColor =  '#D3D3D3' ;
+    document.getElementById("TRUE"+int).style.backgroundColor =  '#D3D3D3';
 
     var x = document.getElementById("false"+int).value;
     document.getElementById("demo"+int).innerHTML = "";
     $("#demo"+int).append(x);
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]) 
+}
+
+
+function choix_multiple(int,bools) {
+	result = true;
+	for (i = 0; i < bools.length; i++){
+		if (bools[i]) {
+			result = result && (document.pythonqcm.choix[i].checked);
+		}
+		else {
+			result = result && !(document.pythonqcm.choix[i].checked);
+		}
+	} 
+	if (result) {
+		document.getElementById("box"+int).style.backgroundColor ='#00FF00';
+		var x = document.getElementById("true"+int).value;
+    	document.getElementById("demo"+int).innerHTML = "";
+    	$("#demo"+int).append(x);
+    	MathJax.Hub.Queue(["Typeset",MathJax.Hub]) 
+	} else {
+		document.getElementById("box"+int).style.backgroundColor ='#FF0000';
+		var x = document.getElementById("false"+int).value;
+    	document.getElementById("demo"+int).innerHTML = "";
+    	$("#demo"+int).append(x);
+    	MathJax.Hub.Queue(["Typeset",MathJax.Hub]) 
+	}
 }
