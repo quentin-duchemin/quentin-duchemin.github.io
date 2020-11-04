6b694d1be35bf242c52d13317436fe2ec4a52f7e
quentin-duchemin <quentin.duchemin@eleves.enpc.fr>
2020-11-04 23:57:27 +0100

    update

--- quizz/applications.js
+++ quizz/applications.js
@@ -2,8 +2,8 @@ function myFunction(int) {
   	document.getElementById("TRUE"+int).style.backgroundColor =  '#00FF00' ;
   	document.getElementById("FALSE"+int).style.backgroundColor =  '#D3D3D3' ;
     var x = document.getElementById("true"+int).value;
-    document.getElementById("demo"+int).innerHTML = "";
-    $("#demo"+int).append(x);
+    document.getElementById("message"+int).innerHTML = "";
+    $("#message"+int).append(x);
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]) 
 }
 function myFunction2(int) {
@@ -12,8 +12,8 @@ function myFunction2(int) {
     document.getElementById("TRUE"+int).style.backgroundColor =  '#D3D3D3';
 
     var x = document.getElementById("false"+int).value;
-    document.getElementById("demo"+int).innerHTML = "";
-    $("#demo"+int).append(x);
+    document.getElementById("message"+int).innerHTML = "";
+    $("#message"+int).append(x);
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]) 
 }
 
@@ -22,23 +22,23 @@ function choix_multiple(int,bools) {
 	result = true;
 	for (i = 0; i < bools.length; i++){
 		if (bools[i]) {
-			result = result && (document.pythonqcm.choix[i].checked);
+			result = result && (document.getElementById("qcm"+int).choix[i].checked);
 		}
 		else {
-			result = result && !(document.pythonqcm.choix[i].checked);
+			result = result && !(document.getElementById("qcm"+int).choix[i].checked);
 		}
 	} 
 	if (result) {
 		document.getElementById("box"+int).style.backgroundColor ='#00FF00';
 		var x = document.getElementById("true"+int).value;
-    	document.getElementById("demo"+int).innerHTML = "";
-    	$("#demo"+int).append(x);
+    	document.getElementById("message"+int).innerHTML = "";
+    	$("#message"+int).append(x);
     	MathJax.Hub.Queue(["Typeset",MathJax.Hub]) 
 	} else {
 		document.getElementById("box"+int).style.backgroundColor ='#FF0000';
 		var x = document.getElementById("false"+int).value;
-    	document.getElementById("demo"+int).innerHTML = "";
-    	$("#demo"+int).append(x);
+    	document.getElementById("message"+int).innerHTML = "";
+    	$("#message"+int).append(x);
     	MathJax.Hub.Queue(["Typeset",MathJax.Hub]) 
 	}
 }
